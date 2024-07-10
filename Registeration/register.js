7a95d493f6a736bf788861f9844b0f4fe1af43e3
Ericode254 <jilloerick6@gmail.com>
2024-07-10 20:30:25 +0300

    made a few changes

---
+++ Registeration/register.js
@@ -0,0 +1,7 @@
+let user_name = document.getElementById("Username");
+let first_name = document.getElementById("firstname");
+let last_name = document.getElementById("lastname");
+let email = document.getElementById("floatingInput");
+let password = document.getElementById("floatingPassword");
+let choice = document.getElementById("selection");
+
