ecfdc1437918b2e56ecfff407c40eb2133eb2946
Ericode254 <jilloerick6@gmail.com>
2024-07-13 07:51:18 +0300

    implimented email functionality in the verification system

--- verification/emailotp.js
+++ verification/emailotp.js
@@ -1,11 +1,39 @@
-let email = document.getElementById("email");
-let otpBtn = document.getElementById("otpbtn");
-let sendOtp = document.getElementById("btn");
+let sendBtn = document.getElementById("sendbtn");
 
-let getEmail = localStorage.getItem("email");
+function sendOtp() {
+    let email = document.getElementById("email");
 
-email.value = getEmail;
+    // let getEmail = localStorage.getItem("email");
+    // email.value = getEmail;
 
-console.log(email.value);
+    let otpVal = Math.floor(Math.random() * 10000);
+    let emailBody = `Your Educafe otp is ${otpVal}`;
+    Email.send({
+        SecureToken: "81f94e24-1159-4ce8-b57a-9bbd714bfee0",
+        To: email.value,
+        From: "jilloerick6@gmail.com",
+        Subject: "Email verification",
+        Body: emailBody
+    }).then(
+        message => {
+            if (message === "OK") {
+                alert("OTP sent to your email " + email.value);
 
-// localStorage.removeItem("email");
+                let otpInp = document.getElementById("otp_inp");
+                let otpBtn = document.getElementById("otpbtn");
+
+                otpBtn.addEventListener("click", () => {
+                    if (otpInp.value === otpVal) {
+                        alert("Email address verified...");
+                        // localStorage.removeItem("email");
+                    } else {
+                        alert("Invalid otp");
+                    }
+                })
+            }
+        }
+    );
+}
+
+
+sendBtn.addEventListener("click", sendOtp);
